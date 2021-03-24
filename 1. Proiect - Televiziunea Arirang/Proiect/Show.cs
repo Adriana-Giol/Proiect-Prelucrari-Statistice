b4fad6ac02155a8dc17cae41ab83ed855990d2cd
Adriana-Giol <gioladriana5@gmail.com>
2021-03-24 10:54:48 +0200

    Create Show.cs

---
+++ 1. Proiect - Televiziunea Arirang/Proiect/Show.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect
+{
+    class Show:ICloneable,IComparable
+    {
+        private string denumire;
+        private float rating;
+        private double venituri;
+        private double 
+        private string categorieVarsta;
+
+        protected string Denumire { get => denumire; set => denumire = value; }
+        protected string CategorieVarsta { get => categorieVarsta; set => categorieVarsta = value; }
+        public float Rating { get => rating; set => rating = value; }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
