d91a5d19660f0aa1830775438ef2ac7e48c12264
Adriana-Giol <gioladriana5@gmail.com>
2021-04-24 02:15:34 +0300

    Create Gospodarie.cs

---
+++ 1. Proiect - Prelucrari Statistice - Recensamantul populatiei 2021/Proiect/Faza 1/Recensamant/Gospodarie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recensamant
+{
+    class Gospodarie
+    {
+        private int codMapa;
+        private int nrPersoane;
+        List<Persoana> listaPersoana = new List<Persoana>();
+
+
+        public Gospodarie(int codMapa, int nrPersoane, List<Persoana> listaPersoana)
+        {
+            this.codMapa = codMapa;
+            this.nrPersoane = nrPersoane;
+            this.listaPersoana = listaPersoana;
+        }
+
+        public int CodMapa { get => codMapa; set => codMapa = value; }
+        public int NrPersoane { get => nrPersoane; set => nrPersoane = value; }
+
+ 
+    }
+}
